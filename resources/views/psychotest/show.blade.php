e631aa18be6a409a2ed4999a93a22bb455ea4fb6
jiro.kirameki <ec2-user@ip-172-31-44-188.us-east-2.compute.internal>
2018-07-06 01:12:55 +0000

    showresultpage

--- resources/views/psychotest/show.blade.php
+++ resources/views/psychotest/show.blade.php
@@ -15,49 +15,42 @@
     
     
 <div class="container">
-  
-     <div class="recomendation">
-        <div class="d-block col-sm-4 bg-primary">
-        <div class="panel panel-info">
-        <div class="panel-heading">
-            <h3 class="panel-title">
-            おすすめのコンテンツ
-            </h3>
-        </div>
-  <div class="panel-body">
-    <ul class="list-unstyled">
-      <li><a href="#">友達リクエストが1件あります</a></li>
-      <li><a href="#">メッセージが3件届いています</a></li>
-      <li><a href="#">今日開催のイベントがあります</a></li>
-    </ul>
-  </div>
-</div></div>
-        </div>
-         <div class="result">
-            <div class="d-block col-sm-4 bg-primary">
-              <div class="panel panel-info">
-  <div class="panel-heading">
-    <h3 class="panel-title">
-            心理テストの詳細結果
-            </h3>
-  </div>
-  <div class="panel-body">
-    <ul class="list-unstyled">
-      <li><a href="#">友達リクエストが1件あります</a></li>
-      <li><a href="#">メッセージが3件届いています</a></li>
-      <li><a href="#">今日開催のイベントがあります</a></li>
-    </ul>
-  </div>
-</div>
-        </div>
-        </div></div>
-    </div>
-    
-   
-        
-        
-         
-            <div class="text-center">
-                <h1>購入したい方はこちら！</h1>
+        <div class="d-block col-sm-5">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                    おすすめのコンテンツ
+                    </h3>
+                </div>
+                <div class="panel-body">
+                    <ul class="list-unstyled">
+                      <li><a href="#">友達リクエストが1件あります</a></li>
+                      <li><a href="#">メッセージが3件届いています</a></li>
+                      <li><a href="#">今日開催のイベントがあります</a></li>
+                    </ul>
+                </div>
             </div>
+        </div>
+    <div class="result">
+        <div class="d-block col-sm-offset-2 col-sm-5"> 
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        心理テストの詳細結果
+                    </h3>
+                </div>
+                <div class="panel-body">
+                    <ul class="list-unstyled">
+                        <li><a href="#">友達リクエストが1件あります</a></li>
+                        <li><a href="#">メッセージが3件届いています</a></li>
+                        <li><a href="#">今日開催のイベントがあります</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="text-center">
+    <button class="btn btn-success btn-lg" type="submit">購入したい方はこちら！</button>
+</div>
 @endsection
