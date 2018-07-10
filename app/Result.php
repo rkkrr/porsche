9b10877a16ac8a62c78f2a8b8dc181de984994b7
saki.ota <saki.a.ota@rakuten.com>
2018-07-10 06:19:26 +0000

    fixing trouble on heroku

--- app/Result.php
+++ app/Result.php
@@ -6,12 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Result extends Model
 {
-<<<<<<< HEAD
-    
-=======
     public function score(){
-        $result =intval($data->q1)+intval($data->q2)+intval($data->q3);
  
     }
->>>>>>> score done
 }
