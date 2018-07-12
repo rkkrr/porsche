d989ec4d5eb18a4c53b0d433c656217984f89b6a
jiro.kirameki <ec2-user@ip-172-31-44-188.us-east-2.compute.internal>
2018-07-12 01:25:05 +0000

    fresh

--- app/Result.php
+++ app/Result.php
@@ -6,16 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Result extends Model
 {
-<<<<<<< HEAD
 
     public function score(){
         $result =intval($data->q1)+intval($data->q2)+intval($data->q3);
  
     }
     
-=======
-    public function score(){
- 
-    }
->>>>>>> 9b10877a16ac8a62c78f2a8b8dc181de984994b7
 }
