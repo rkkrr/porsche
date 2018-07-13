6250c90f4b75320567435ea05142b220bef8681a
natsuhiko.nishino <ec2-user@ip-172-31-4-211.us-east-2.compute.internal>
2018-07-13 01:37:27 +0000

    natsuhiko

--- resources/views/psychotest/index.blade.php
+++ resources/views/psychotest/index.blade.php
@@ -4,15 +4,13 @@
        
         <div class="center jumbotron">
             <div class="text-center">
-                <h1>ひとりぼっちのみなさん、こんにちは。</h1>
-            
+                <h1>こんにちは。</h1>
             </div>
-            
         </div>
         
         <div class="text-center">
-         <div class = btn btn-lg btn-primary >
-            <a href="test">テストを受ける！</a>
-         </div>     
+            <div class = btn btn-lg btn-primary >
+                <a href="test">テストを受ける！</a>
+            </div>     
         </div>
 @endsection
