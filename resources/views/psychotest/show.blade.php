b6e23d92acdd870c78dd38e80eba31f123310baf
saki.ota <saki.a.ota@rakuten.com>
2018-07-19 07:26:29 +0000

    fix issue

--- resources/views/psychotest/show.blade.php
+++ resources/views/psychotest/show.blade.php
@@ -30,21 +30,9 @@
         
         <div class="text-center">
         <div class="picture">
-            <?php
-              if($result <= 25){ 
-                        print <img src='images/houou.png' style='width:250px;'>}
-                    else if($result <= 50){ 
-                        print <img src='images/ookami.png' style='width:250px;'>}
-                    else if($result <= 75){ 
-                        print <img src='images/deer image.jpg' style='width:250px;'>}
-                    else if($result <= 100){ 
-                        print <img src='images/sheep image.jpg' style='width:250px;'>}
-                    else ($result <= 120){ 
-                        print <img src='hamster.png' style='width:250px;'> }
-
-                        ?>
-            }
-                        
+           
+                        <img src='images/houou.png' style='width:250px;'>}
+                   
         </div>
         </div>
         
