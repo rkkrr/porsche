8f73e60eba3aad38da6b4623124a6d025aab26db
gi-no <ec2-user@ip-172-31-0-74.us-east-2.compute.internal>
2018-07-23 01:15:15 +0000

    finishstore

--- resources/views/psychotest/test2.blade.php
+++ resources/views/psychotest/test2.blade.php
@@ -9,10 +9,10 @@
     <br>
     <div style="font-size:20pt; display:inline-block;">いま何食べたい気分？</div>
    {{Form::select('age',  [
-     '0' => '中華',
-     '5' => 'イタリアン',
-     '10' => '和食',
-     '15' => '飲み'], null, ['class' => 'btn btn-primary btn-lg dropdown-toggle']
+     '1' => '中華',
+     '2' => 'イタリアン',
+     '3' => '和食',
+     '4' => '飲み'], null, ['class' => 'btn btn-primary btn-lg dropdown-toggle']
   )}}
   <br><br><br><br><br><br>
   {!! Form::submit('同じ気分の人を見てみよう！', ['class' => 'btn btn-lg btn-primary']) !!}
