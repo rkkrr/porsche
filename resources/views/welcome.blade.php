7db7e7c8a3a2458058371c3b1e4d235bbe8e7c46
jiro.kirameki <ec2-user@ip-172-31-18-27.us-east-2.compute.internal>
2018-07-24 05:47:49 +0000

    top button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,13 +8,13 @@
             <h1></h1>
  
             <div class="col-md-offset-10">
-            {!! link_to_route('signup.get', 'はじめての方はこちら', null, ['class' => 'square_btn']) !!}
+            {!! link_to_route('signup.get', 'はじめての方はこちら', null, ['class' => 'cp_btn']) !!}
             </div>
             
             <br><br><br><br>
             
             <div class="col-md-offset-10">
-            {!! link_to_route('login.post', 'ログイン', null, ['class' => 'square_btn']) !!}
+            {!! link_to_route('login.post', 'ログイン', null, ['class' => 'cp_btn']) !!}
             </div>
         </div>
     
