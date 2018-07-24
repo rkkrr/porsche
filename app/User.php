d0a0c8b4b780b0c7efb00a5b4ecd214ab85b57f6
saki.ota <saki.a.ota@rakuten.com>
2018-07-24 07:10:33 +0000

    foodtype fix

--- app/User.php
+++ app/User.php
@@ -5,6 +5,9 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use App\Food;
+use App\Result;
+
 class User extends Authenticatable
 {
     use Notifiable;
