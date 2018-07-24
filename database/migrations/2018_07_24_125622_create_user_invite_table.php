8860c8c5522ae51d3d779018e3b2dab25b373afa
saki.ota <saki.a.ota@rakuten.com>
2018-07-24 05:54:55 +0000

    conflict solving1

--- database/migrations/2018_07_24_125622_create_user_invite_table.php
+++ database/migrations/2018_07_24_125622_create_user_invite_table.php
@@ -13,11 +13,7 @@ class CreateUserInviteTable extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD:database/migrations/2018_07_24_125622_create_user_invite_table.php
        Schema::create('user_invite', function (Blueprint $table) {
-=======
-        Schema::create('user_invite', function (Blueprint $table) {
->>>>>>> 67828843429c525776d0cd23c2af3ee6b9b7257e:database/migrations/2018_07_23_154401_create_user_invite_table.php
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
             $table->integer('invite_id')->unsigned()->index();
