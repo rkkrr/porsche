c120c3ee1d64e2c8c98bac425ecc97d99dd08d6c
saki.ota <saki.a.ota@rakuten.com>
2018-07-24 05:33:24 +0000

    merge chika3

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');});
 
 
-Route::get('/home', 'MainController@index')->name('psychotest.home');
+Route::get('home', 'MainController@index')->name('psychotest.home');
 Route::post('answer', 'MainController@store')->name('answer.store');
 
 
