fbddde0c0fd358214aa416478ae617771c8a0511
natsuki-yoshida <ec2-user@ip-172-31-34-30.us-east-2.compute.internal>
2018-07-25 01:10:33 +0000

    CSS

--- resources/views/newproduct/restaurant.blade.php
+++ resources/views/newproduct/restaurant.blade.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
 <html>
     <link rel="stylesheet" href="{{ secure_asset('css/result.css') }}">
-    <script src="https://code.jquery.com/jquery-1.12.4.min.js"
-            integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
-            crossorigin="anonymous"></script>
  @extends('layouts.app')
  
  @section('content')
@@ -11,7 +8,6 @@
  $user = Auth::user();
 ?>
 
- <div class="container">
     <div class="text-center">
        <h3>あなたと同じものを食べたい同期は・・・？</h3>
     </div>
@@ -25,29 +21,37 @@
         @endforeach
     </div>
         <br><br><br><br><br>
-    <div class="result">
+
         <div class="ribbon">
-            <h3>そんなあなたへのオススメ</h3>
-        <div class="d-inline-block col-sm-offset-1 col-sm-5"> 
-        <div class="panel panel-danger">
-        <div class="panel-heading">
-        <h3 class="panel-title">レストラン</h3>
+            <strong><h3>そんなあなたへのオススメ</h3></strong>
         </div>
+<br><br>
+        <div class="d-inline-block col-sm-offset-1 col-sm-5">
+        <div class="move_center"> 
+        <div class="panel panel-danger">
+            <div class="panel-heading">
+                <h3 class="panel-title">レストラン</h3>
+            </div>
             <div class="panel-body">
                 @include ('psychotest.restaurant2')
             </div>
         </div>
         </div>
-    <div class="d-block  col-sm-5">
-        <div class="panel panel-danger">
-        <div class="panel-heading">
-            <h3 class="panel-title">レシピ</h3>
         </div>
-            <div class="panel-body">
-              @include ('psychotest.restaurant3')  
+
+        
+    <div class="d-block  col-sm-5">
+        <div class="move_center">
+        <div class="panel panel-danger">
+            <div class="panel-heading">
+                <h3 class="panel-title">レシピ</h3>
             </div>
-             </div>
+            <div class="panel-body">
+                @include ('psychotest.restaurant3')  
+            </div>
+        </div>
+        </div>
     </div>
-</div>
+
 @endsection
 </html>
